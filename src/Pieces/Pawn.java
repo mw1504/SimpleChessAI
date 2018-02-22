731a75d32dab11d2bac5e73594f90f0137055f26
mw1504 <33066823+mw1504@users.noreply.github.com>
2018-02-22 16:14:32 +0000

    Inclusion of movement of two spaces forwards.

--- src/Pieces/Pawn.java
+++ src/Pieces/Pawn.java
@@ -3,16 +3,35 @@ package Pieces;
 import javafx.util.Pair;
 
 public class Pawn extends Pieces {
+    private boolean firstMove;
 
     public Pawn(String Colour, Pair<Integer, Integer> coord) {
         super("Pawn", Colour, coord);
+        firstMove = true;
     }
+    
+    /**
+     * A Pawn has a special case of movement where on it's first move it can move one further.
+     * @return boolean representing whether the move is valid or not
+     */
+    public boolean firstMove() {
+        if (firstMove) {
+            try {
+                this.setX(this.getX() + 2);
+            } catch (IllegalArgumentException e) {
+                return false;
+            }
+            firstMove = false;
+            return true;
+        }
+    }    
 
     /**
-     * A pawn may only move forwards.
+     * A Pawn may only move forwards.
      * @return boolean representing whether the move is valid or not
      */
     public boolean move() {
+        firstMove = false;
         try {
             this.setX(this.getX() + 1);
         } catch (IllegalArgumentException e) {
@@ -22,7 +41,7 @@ public class Pawn extends Pieces {
     }
 
     /**
-     * A pawn may only take pieces that are diagonally in front of it by one space
+     * A Pawn may only take pieces that are diagonally in front of it by one space
      * @param direction whether the attack is to the left or the right
      * @return boolean representing whether the move is valid or not
      */
